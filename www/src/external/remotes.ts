95dcb49f25494b915956aab516deb4d9df5bd2b8
Aleksey Medvedev <al.medvedev@corp.vk.com>
2022-02-07 22:18:57 +0700

    Added lg protocol and remote

--- www/src/external/remotes.ts
+++ www/src/external/remotes.ts
@@ -4,6 +4,9 @@ import SonyProtocolJSON from 'src/external/protocols/sony.json';
 import TizenRemoteJSON from 'src/external/remotes/samsung.json';
 import TizenProtocolJSON from 'src/external/protocols/samsung.json';
 
+import LGRemoteJSON from 'src/external/remotes/lg.json';
+import LGProtocolJSON from 'src/external/protocols/lg.json';
+
 import { Remote, RemoteJSON, ProtocolJSON } from 'src/types/remote';
 
 const remotes: Record<string, Remote> = {
@@ -20,6 +23,13 @@ const remotes: Record<string, Remote> = {
     label: (SonyRemoteJSON as RemoteJSON).label,
     layout: (SonyRemoteJSON as RemoteJSON).layout,
     keycodes: (SonyProtocolJSON as ProtocolJSON).keyMap
+  },
+  [LGRemoteJSON.id]: {
+    id: LGRemoteJSON.id,
+    protocol: (LGProtocolJSON as ProtocolJSON).protocol,
+    label: (LGRemoteJSON as RemoteJSON).label,
+    layout: (LGRemoteJSON as RemoteJSON).layout,
+    keycodes: (LGProtocolJSON as ProtocolJSON).keyMap
   }
 }
 
